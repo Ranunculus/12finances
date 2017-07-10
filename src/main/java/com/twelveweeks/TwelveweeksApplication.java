5bf51410c584d1e8896fb8e787fa4b144c87ea17
Ranunculus <tatianamalyutina@gmail.com>
2017-07-11 00:02:54 +0300

    init commit

---
+++ src/main/java/com/twelveweeks/TwelveweeksApplication.java
@@ -0,0 +1,12 @@
+package com.twelveweeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveweeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwelveweeksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/twelveweeks/TwelveweeksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twelveweeks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwelveweeksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
