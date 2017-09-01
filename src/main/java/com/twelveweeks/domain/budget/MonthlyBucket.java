19a699569f0333e380e77174bff516d7e76f27c6
vfalkenberg@bk.ru <134679>
2017-09-01 23:20:28 +0600

    Delete lombok

--- src/main/java/com/twelveweeks/domain/budget/MonthlyBucket.java
+++ src/main/java/com/twelveweeks/domain/budget/MonthlyBucket.java
@@ -1,16 +1,8 @@
 package com.twelveweeks.domain.budget;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class MonthlyBucket{
     private String name;
     private BigDecimal limit;
--- src/main/java/com/twelveweeks/domain/transactions/Expenses.java
+++ src/main/java/com/twelveweeks/domain/transactions/Expenses.java
@@ -1,17 +1,64 @@
 package com.twelveweeks.domain.transactions;
 
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-import lombok.extern.java.Log;
+
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name = "EXPENSE")
 
-@Log
-@AllArgsConstructor
-@ToString
+
 public class Expenses extends Transaction {
+    @Override
+    public BigDecimal getValue() {
+        return null;
+    }
+
+    @Override
+    public void setValue(BigDecimal value) {
+
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public void setType(String type) {
+
+    }
+
+    @Override
+    public Date getDate() {
+        return null;
+    }
+
+    @Override
+    public void setDate(Date date) {
+
+    }
+
+    @Override
+    public Integer getUserId() {
+        return null;
+    }
+
+    @Override
+    public void setUserId(Integer userId) {
+
+    }
+
+    @Override
+    public Category getCategory() {
+        return null;
+    }
+
+    @Override
+    public void setCategory(Category category) {
+
+    }
 }
