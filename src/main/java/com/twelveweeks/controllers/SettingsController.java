3f73e310e2f8bfc2423c72b5a6048430612f192e
Ranunculus <tatianamalyutina@gmail.com>
2018-04-26 23:07:21 +0300

    settings: add category

--- src/main/java/com/twelveweeks/controllers/SettingsController.java
+++ src/main/java/com/twelveweeks/controllers/SettingsController.java
@@ -1,17 +1,38 @@
 package com.twelveweeks.controllers;
 
+import com.twelveweeks.domain.enums.TransactionType;
+import com.twelveweeks.domain.transactions.Category;
+import com.twelveweeks.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @Controller
 @RequestMapping("/settings")
 public class SettingsController {
 
+    @Autowired
+    public CategoryRepository categoryRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "settings";
+    }
+
     @PostMapping("/category/add")
-    @ResponseBody
-    public String addCategory(String categoryName) {
-        return "ok";
+    public String addCategory(@RequestParam TransactionType transactionType,
+                              @RequestParam String categoryName,
+                              @RequestParam BigDecimal limit) {
+        Category category = new Category();
+        category.setLimit(limit);
+        category.setName(categoryName);
+        // TODO: 26/04/18
+//        category.setTransactionType(transactionType);
+        categoryRepository.save(category);
+        return "redirect:/settings";
     }
 }
