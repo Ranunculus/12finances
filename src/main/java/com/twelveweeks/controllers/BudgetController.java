39b92903bf251d585541b2dfbfb6a35e211a0715
Ranunculus <tatianamalyutina@gmail.com>
2018-05-20 16:34:51 +0300

    budget: totals by category

--- src/main/java/com/twelveweeks/controllers/BudgetController.java
+++ src/main/java/com/twelveweeks/controllers/BudgetController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/budget")
@@ -40,21 +42,79 @@ public class BudgetController {
     }
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
 
+        List<Totals> totals = new ArrayList<>();
         Iterable<Category> all = categoryRepository.findAll();
         for (Category category : all) {
+            Totals categoryTotal = new Totals(category.getName());
             Iterable<Expenses> expenses = expensesRepository.findByCategory(category);
             BigDecimal total = BigDecimal.ZERO;
             for (Expenses expense : expenses) {
                 total = total.add(expense.getValue());
             }
+            categoryTotal.setSpent(total);
             Budget budget = budgetRepository.findByCategory(category);
             if (budget != null) {
-                System.out.println(budget.getAmount().subtract(total));
+                categoryTotal.setLimit(budget.getAmount());
+                categoryTotal.setLeft(budget.getAmount().subtract(total));
             }
+            totals.add(categoryTotal);
+
         }
-        return "budget";
+        model.addAttribute("totals", totals);
+        return PAGE_NAME;
+    }
+
+    // TODO: 20/05/18 separate class
+    private class Totals {
+        private String categoryName;
+        private BigDecimal limit;
+        private BigDecimal spent;
+        private BigDecimal left;
+
+        public Totals(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        public Totals(String categoryName, BigDecimal limit, BigDecimal spent, BigDecimal left) {
+            this.categoryName = categoryName;
+            this.limit = limit;
+            this.spent = spent;
+            this.left = left;
+        }
+
+        public String getCategoryName() {
+            return categoryName;
+        }
+
+        public void setCategoryName(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        public BigDecimal getLimit() {
+            return limit;
+        }
+
+        public void setLimit(BigDecimal limit) {
+            this.limit = limit;
+        }
+
+        public BigDecimal getSpent() {
+            return spent;
+        }
+
+        public void setSpent(BigDecimal spent) {
+            this.spent = spent;
+        }
+
+        public BigDecimal getLeft() {
+            return left;
+        }
+
+        public void setLeft(BigDecimal left) {
+            this.left = left;
+        }
     }
 
 }
