2a412c304befc9730458ccebf0b6cbfefa638f50
Ranunculus <tatianamalyutina@gmail.com>
2018-06-13 22:17:40 +0300

    settings: restrict budget duplicates

--- src/main/java/com/twelveweeks/controllers/settings/BudgetSettingsController.java
+++ src/main/java/com/twelveweeks/controllers/settings/BudgetSettingsController.java
@@ -2,6 +2,7 @@ package com.twelveweeks.controllers.settings;
 
 import com.twelveweeks.domain.budget.Budget;
 import com.twelveweeks.domain.enums.BudgetType;
+import com.twelveweeks.domain.transactions.Category;
 import com.twelveweeks.repository.BudgetRepository;
 import com.twelveweeks.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,14 @@ public class BudgetSettingsController extends SettingsController {
     public String setBudgets(@RequestParam BudgetType type,
                              @RequestParam BigDecimal amount,
                              @RequestParam Integer categoryId) {
-        Budget budgetObject = new Budget(type, amount, categoryRepository.findById(categoryId).orElse(null));
-        budgetRepository.save(budgetObject);
+        Category category = categoryRepository.findById(categoryId).orElse(null);
+        Budget currentBudget = budgetRepository.findByTypeAndCategory(type, category);
+        if (currentBudget == null) {
+            Budget budgetObject = new Budget(type, amount, category);
+            budgetRepository.save(budgetObject);
+        } else {
+            // TODO: 13/06/18 throw an error
+        }
         return "redirect:/settings";
     }
 
--- src/main/java/com/twelveweeks/repository/BudgetRepository.java
+++ src/main/java/com/twelveweeks/repository/BudgetRepository.java
@@ -1,10 +1,12 @@
 package com.twelveweeks.repository;
 
 import com.twelveweeks.domain.budget.Budget;
+import com.twelveweeks.domain.enums.BudgetType;
 import com.twelveweeks.domain.transactions.Category;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BudgetRepository extends CrudRepository<Budget, Integer> {
 
     Budget findByCategory(Category category);
+    Budget findByTypeAndCategory(BudgetType type, Category category);
 }
